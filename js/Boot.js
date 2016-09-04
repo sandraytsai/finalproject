8488456503d6405ebfb7a4fe76b2c1090f99f852
Vishal Tiwari <vishal.tiwari.09@gmail.com>
2016-09-03 21:06:48 -0400

    Being fussy and formatting for personal readability.

--- js/Boot.js
+++ js/Boot.js
@@ -1,12 +1,19 @@
 var boot = function(game){
+
   console.log("%cStarting my awesome game", "color:white; background:red");
 };
-  
+
+
 boot.prototype = {
+
+
   preload: function(){
-    this.game.load.image("loading","assets/preloader-bar.png"); 
+
+    this.game.load.image("loading","assets/preloader-bar.png");
   },
-    create: function(){
+
+  create: function(){
+
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.game.state.start("Preload");
