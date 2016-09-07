61ce5c73b49ccb6bfc2789619d3c34ee9fe5ded2
Sandra Tsai <sandraytsai@gmail.com>
2016-09-07 00:29:15 +0000

    intro placeholder animation

--- js/states/Intro.js
+++ js/states/Intro.js
@@ -5,10 +5,22 @@ var intro = function(game){
 intro.prototype = {
   create: function(){
     var background = this.game.add.tileSprite(0, 0, 5000, 1920, 'background');
+    var block = this.add.image(700, 300, '50x300');
+    player = this.game.add.sprite(5, 770, 'player');
+    this.physics.arcade.enable(player);
+    createPlayer(player);
+    player.body.velocity.x = 100;
+    player.animations.play('right');
     var gameself = this
     // animation goes here
     setTimeout(function() {
       gameself.game.state.start("Game");
-    }, 500)
+    }, 9000)
+  },
+
+  update: function(){
+    if (player.x > 720) {
+      player.kill();
+    };
   }
 };
